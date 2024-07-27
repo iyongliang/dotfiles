fe8684e53e03f1a307c260696f695650c5c5d140
yongliang <neeyongliang@gmail.com>
2024-07-27 20:49:17 +0800

    Update: add nvim install guide

--- software/nvim/init.lua
+++ software/nvim/init.lua
@@ -1,3 +1,14 @@
+-- sudo add-apt-repository ppa:neovim-ppa/unstable
+-- sudo apt update
+-- sudo apt install neovim
+-- or
+-- echo "deb https://ppa.launchpadcontent.net/neovim-ppa/stable/ubuntu focal main" > /etc/apt/sources.list.d/nvim-focal.list
+-- sudo apt-key adv \
+--   --keyserver hkp://keyserver.ubuntu.com:80 \
+--   --recv-keys 9DBB0BE9366964F134855E2255F96FCF8231B6DD
+-- sudo apt update
+-- sudo apt install neovim
+
 require("vim.lsp.health").check()
 
 -- load options
